4d1efd3b11ec0960d44bbd06eb220cf0664b3bdf
Nick Tartaro <ntartaro@gmail.com>
2018-09-21 16:30:54 -0400

    even more framework

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const hbs = require("hbs");
-const bodyParser = require("body-parser");
-
+const express = require("express")
+const hbs = require("hbs")
+const bodyParser = require("body-parser")
 const app = express()
 
-app.listen(3000, () => {
-    
-})
+app.use(require("./routes/index.js"))               //imports routes
+app.use(bodyParser.urlencoded({ extended: true}))   //handles form submissions
+app.set('view engine', 'hbs')                       //sets Handlebars as view engine
+
+app.listen(3000, () => {})
